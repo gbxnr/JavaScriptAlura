ba9e7e9bdbdea8091524bb4d90040f71b4249646
gbxnr <gbxnr25@gmail.com>
2026-06-23 21:40:43 -0300

    aprendendo variaveis

---
+++ hoisting.js
@@ -0,0 +1,5 @@
+console.log('minha idade é: ', idade)
+
+var idade = 36
+
+console.log("minha idade atual depois de declarar : ", idade)
---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+//formas diferentes de trabalhar com javascript
+// variavel global
+// 1 - var
+// 2 - les
+// 3 - const
+console.log("minha idade atual é:")
+
+var idade = 18
+var dataDeAniversario = '25 sw junhoo'
+    
+console.log(idade)
+
+console.log("minha idade depois do meu aniversario é: " + idade)
+console.log("sua data de aniversario é :" + dataDeAniversario)
+
+
+
