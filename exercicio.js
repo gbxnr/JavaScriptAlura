10e1981734f47038367be660bb1a72048ec1bbca
GGLTech <devmycrosaas@gmail.com>
2026-06-25 19:11:09 -0300

    realizando alguns exercicios praticos

---
+++ exercicio.js
@@ -0,0 +1,35 @@
+
+
+let nome = "Gabriel";
+console.log("Olá, " + nome + " Seja bem vindo ao curso de JavaScript");
+
+let anoAtual = 2026;
+let anoNascimento = 2007;
+
+var soma = anoAtual - anoNascimento;
+console.log("você tem " + soma + " anos" );
+
+let cidade = "Brasilia";
+let estado = "DF";
+let pais = "Brasil";
+
+console.log("Você está em " + cidade + " - " + estado + "," + pais+ ".");
+
+let temCarteira = true;
+console.log(typeof temCarteira);
+
+let saldo = 0;
+let deposito = 200;
+console.log(deposito);
+let saque = 50;
+let saldoFinal = deposito - saque;
+console.log("saldo depois do saque: " + saldoFinal);
+
+let matematica = 6.0;
+let portugues = 7.0;
+let ciencias = 8.0;
+
+let somaNotas = matematica + portugues + ciencias;
+let media = somaNotas /2;
+console.log("Sua media final é: " + media)
+
