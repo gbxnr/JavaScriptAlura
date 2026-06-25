1a437c8b19af4468a6b661232c4c1f2987503eec
GGLTech <devmycrosaas@gmail.com>
2026-06-25 19:23:09 -0300

    pratiquei alguns dos opreadores matematicos do javascript

---
+++ scripts/operadoresMatematicos.js
@@ -0,0 +1,22 @@
+const numeroUm = 4;
+const numeroDois = 8;
+
+const subtracao = numeroUm - numeroDois;
+
+console.log('Subtração:', subtracao);
+
+const soma = numeroUm + numeroDois;
+console.log('soma: ', soma);
+
+const divisao = numeroUm / numeroDois;
+console.log('Divisão: ', divisao);
+
+const multiplicacao = numeroUm * numeroDois;
+console.log('Multiplicação: ', multiplicacao);
+
+const resto = 12% 2;
+console.log('Resto da divisao:', resto)
+
+const potencia = 2 ** 3;
+console.log('Potencia: ', potencia)
+
