b4460f8aa95829a3089d4c282f88578ed78413b9
GGLTech <devmycrosaas@gmail.com>
2026-06-25 16:16:45 -0300

    aprendnedo tipos de variaveis

---
+++ tipos.js
@@ -0,0 +1,19 @@
+let nome = 'ana'
+let idade = 23
+let altura = 1.67
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof altura)
+
+let estaChovendo = false
+
+console.log(typeof estaChovendo)
+
+let cidade
+
+console.log(typeof cidade)
+
+let jaAprendeuJS = null
+
+console.log(typeof jaAprendeuJS)
