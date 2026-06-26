caf811d466f68fa78ed6e144556a9ff3a2ec03b9
GGLTech <devmycrosaas@gmail.com>
2026-06-26 19:41:16 -0300

    finalizei a compreensão dos operadores de comparação

---
+++ scripts/operadoresComparacao.js
@@ -0,0 +1,23 @@
+const idadeVinny = 36
+const minhaIdade = 35
+
+const comparacao = idadeVinny == minhaIdade
+//console.log('São iguais: ', comparacao)//
+
+//const comparacao = idadeVinny < minhaIdade
+//console.log('São iguais: ', comparacao)//
+
+//const comparacao = idadeVinny > minhaIdade
+//console.log('São iguais: ', comparacao)
+
+//const comparacao = idadeVinny < minhaIdade
+console.log("São Iguais? ", comparacao)
+
+const idade = 36
+const idadeIrmao = 16
+
+const souMaiorDeIdade = idade >= 18
+const irmaoMaiorDeIdade = idadeIrmao >= 18
+
+console.log( 'Sou maior de idade?  ', souMaiorDeIdade)
+console.log('Meu irmão é maior de idade? ', irmaoMaiorDeIdade)
