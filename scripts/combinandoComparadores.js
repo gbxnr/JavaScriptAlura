dd5bf5323abb81393f14cceb1b80e21d8a2deb99
GGLTech <devmycrosaas@gmail.com>
2026-06-26 19:51:41 -0300

    combinando os operadores utilizando AND, OR, NOT

---
+++ scripts/combinandoComparadores.js
@@ -0,0 +1,18 @@
+const idade = 18
+const maiorDeIdade = idade>= 18
+const possuiCNH = false
+
+const podeDirigir = maiorDeIdade && possuiCNH  
+console.log('Pode dirigir?', podeDirigir)
+
+const podeViajarSozinha = maiorDeIdade || possuiCNH
+console.log('Pode viajar sozinha? ',  podeViajarSozinha)
+
+const precisaDeAcompanhante = !maiorDeIdade 
+console.log('Precisa de acompanhante? ', precisaDeAcompanhante)
+
+// and &&
+// or ||
+// not !
+
+maiorDeIdade && possuiCNH || !precisaDeAcompanhante
