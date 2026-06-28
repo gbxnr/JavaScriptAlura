82f66b8561d1709dbdcb5497f2c6facc8175d3f9
GGLTech <devmycrosaas@gmail.com>
2026-06-28 10:45:23 -0300

    adiconei um arquivo html e conectei ao javascript com um windows prompt buscando o resultado das condicionais no inspecionar no navegador

--- scripts/condicionais.js
+++ scripts/condicionais.js
@@ -1,11 +1,22 @@
-const idade = 17
+//const idade = 17
+//if(idade >= 18){
+   // console.log('é maior de idade! ')
+//}else{
+    //console.log('é menor de idade! ')
+//}
 
+const notaDoAluno = 3
 
-
-if(idade >= 18){
-    console.log('é maior de idade! ')
+if(notaDoAluno >= 9){
+    console.log('nota excelente')
+} else if(notaDoAluno >= 7){
+    console.log('Boa Nota')
+}else if(notaDoAluno >=4){
+    console.log('nota mediana')
 }else{
-    console.log('é menor de idade! ')
+    console.log('Nota ruim')
 }
+
+
 
 
