09957300499945073cfd6e7e8d27f09f5a8eac44
GGLTech <devmycrosaas@gmail.com>
2026-06-29 17:27:40 -0300

    trabalhei com laços de repetição e fiz um pequeno teste de impa ou par

--- scripts/lacoFor.js
+++ scripts/lacoFor.js
@@ -1,3 +1,26 @@
+
+//Estrutura laços de repetição for
 for(let contador = 1; contador <= 10; contador++){
   console.log('número atual:', contador)
+}
+
+for(let numero =0; numero <= 15; numero++){
+  if (numero % 2 == 0) {
+    console.log("numero par encontrado", numero)
+  }
+}
+
+for(let numero =0; numero <= 15; numero++){
+  if (numero % 2 > 0) {
+    console.log("numero impar encontrado", numero)
+  }
+}
+
+const palavra = 'calopsita'
+
+// palavra.length indica quantos caracteres uma string possui
+// palavra[1] - pega a letra na posição indicada
+
+for(let cont = 0; cont < palavra.length; cont++){ // esse lengrh servira para saber o tamano e quantas letras tem a plavra
+   console.log(palavra[cont])
 }
