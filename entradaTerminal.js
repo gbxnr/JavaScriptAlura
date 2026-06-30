51aaf58563d4ac1eb95b2f2303e70ddf24a2b6f6
gbxnr <gbxnr25@gmail.com>
2026-06-30 14:15:05 -0300

    entrada de dados com  uma sintaxe completa que chega a ser um pouco semelhante ao do java

--- entradaTerminal.js
+++ entradaTerminal.js
@@ -1,11 +1,12 @@
 const readline = require ('readline')  // biblioteca que interage com o terminal
 
-const leitor = readline.createInterfac({
+const leitor = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
 
 leitor.question('Qual é o seu nome? ', (nome) => {
     console.log(' Olá, ', nome)
-    console.log()
+    console.log(', é um prazer em te conhecer!')
+    leitor.close()
 })
