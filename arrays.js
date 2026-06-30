75f8ad78a8c8845ad3ed8dc915b3707ab363ab5b
GGLTech <devmycrosaas@gmail.com>
2026-06-30 18:21:34 -0300

    inicializei um aprendimento basico sobre arrays pois ja tenho uma boa base vinda de outras linguagens

---
+++ arrays.js
@@ -0,0 +1,14 @@
+const frutas = ['uva', 'banana', 'abacaxi', 'maça', 'morango']
+
+console.log(frutas)
+
+console.log('Primeira fruta: ', frutas[0])
+console.log('Ultima fruta: ', frutas[4])
+
+console.log('total de frutas: ', frutas.length)
+
+frutas.push('melancia')
+
+console.log('total de frutas ao adicionar mais fruta ', frutas.length)
+
+console.log('ultima fruta:', frutas[frutas.length-1])
