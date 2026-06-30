6b25a61a68dd03a524475cab907075e4a4efe670
gbxnr <gbxnr25@gmail.com>
2026-06-30 09:13:32 -0300

    com o settimeout eu aprendi que ele vaie xecutar determinada tarefa depois de um tempo escolhido por vc e o setInterval vi realizar uma execução ate o tempo em que v permitir que ele execute

--- funcoesGlobais.js
+++ funcoesGlobais.js
@@ -4,3 +4,14 @@ function saudacao (){
 
 setTimeout(saudacao, 2000) //ms // esse setTimeout serve para poder delimitar tempo para executar uma ação
 
+
+let contador = 0
+
+setInterval(() => {
+   contador++
+   console.log(' tempo decorrido (em segundos)', contador)
+
+   if(contador == 10){
+    clearInterval(id)
+   }
+}, 1000)
